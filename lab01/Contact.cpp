48029e4b4c2c07395e19dbd6b35c47881642184d
eofid <arhipadynec@gmail.com>
2024-09-13 13:13:35 +0300

    Contact.cpp

--- lab01/Contact.cpp
+++ lab01/Contact.cpp
@@ -1,4 +1,11 @@
-#include "Contact.h"
+class Contact {
+public:
+    std::string name;
+    std::string phone;
+    std::string email;
 
-Contact::Contact(std::string const name, std::string const phone, std::string const email)
-    : name(name), phone(phone), email(email) {}
+    // Конструктор, принимающий параметры по константной ссылке
+    Contact(const std::string& name, const std::string& phone, const std::string& email)
+        : name(name), phone(phone), email(email) {}
+};
+
